5f1b4a1b464a904924651e2f2894cde0a70062ab
Feng <zf934622151@live.com>
2020-09-28 10:16:37 +0800

    init project

---
+++ downloader/src/test/java/cn/windy/crawler/DownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.windy.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
